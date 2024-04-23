e672859275bc3d6accf9fcf539465770076fe6c1
berkeralanli <alanliberker@gmail.com>
2024-04-23 16:37:34 +0300

    sign in pageProps

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,26 +1,28 @@
-import { SignIn } from "@clerk/nextjs";
-import type { AppProps } from "next/app";
-/* eslint-disable react/no-unescaped-entities */
+// pages/_app.tsx
+
+import { AppProps } from 'next/app';
+import { SignIn } from '@clerk/nextjs';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SignIn
-      appearance={{
-        elements: {
-          formButtonPrimary: {
-            fontSize: 14,
-            textTransform: "none",
-            backgroundColor: "#611BBD",
-            "&:hover, &:focus, &:active": {
-              backgroundColor: "#49247A",
+    <>
+      <SignIn
+        appearance={{
+          elements: {
+            formButtonPrimary: {
+              fontSize: 14,
+              textTransform: "none",
+              backgroundColor: "#611BBD",
+              "&:hover, &:focus, &:active": {
+                backgroundColor: "#49247A",
+              },
             },
           },
-        },
-      }}
-    />
+        }}
+      />
+      <Component {...pageProps} />
+    </>
   );
 }
-
-/* eslint-enable react/no-unescaped-entities */
 
 export default MyApp;
